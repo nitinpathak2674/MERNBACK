7caaed0beb5d52cb57f5f11a25e73ff9e87f4f02
Nitin Pathak <pathakn475@example.com>
2026-02-20 20:54:55 +0530

    Cleanup: Removing frontend folders from backend repo

---
+++ src/components/Login.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import "./mix.css";
+
+const Login = () => {
+
+    const [passShow, setPassShow] = useState(false);
+
+    const [inpval, setInpval] = useState({
+        email: "",
+        password: "",
+    });
+
+    const history = useNavigate();
+
+    const setVal = (e) => {
+        const { name, value } = e.target;
+        setInpval((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const loginuser = async(e) => {
+        e.preventDefault();
+
+        const { email, password } = inpval;
+
+        
+
+        if (email === "") {
+            toast.error("Email is required!", { position: "top-center" });
+            return;
+        } else if (!email.includes("@")) {
+            toast.warning("Email must include @!", { position: "top-center" });
+            return;
+        } else if (password === "") {
+            toast.error("Password is required!", { position: "top-center" });
+            return;
+        } else if (password.length < 6) {
+            toast.error("Password must be at least 6 characters!", { position: "top-center" });
+            return;
+        }
+
+        try {
+           const data = await fetch("https://mernback-tdv5.onrender.com/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, password })
+            });
+
+            const res = await data.json();
+
+            if(res.status === 201){
+               
+                localStorage.setItem("usersdatatoken", res.result.token);
+                toast.success("Login successful!", { position: "top-center" });
+                history("/dash");
+                setInpval({ email: "", password: "" });
+            } else {
+                
+                if(res.error){
+                    toast.error(res.error, { position: "top-center" }); 
+                    
+                } else {
+                    toast.error("Login failed. Try again!", { position: "top-center" });
+                }
+            }
+
+        } catch (error) {
+            console.log("Frontend login error:", error);
+            toast.error("Server error. Please try later.", { position: "top-center" });
+        }
+    };
+
+    return (
+        <>
+            <section>
+                <div className="form_data">
+                    <div className="form_heading">
+                        <h1>Welcome Back, Log In</h1>
+                        <p>Hi, we are glad you are back. Please login.</p>
+                    </div>
+
+                    <form>
+                        <div className="form_input">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                value={inpval.email}
+                                onChange={setVal}
+                                name="email"
+                                id="email"
+                                placeholder='Enter Your Email Address'
+                            />
+                        </div>
+                        <div className="form_input">
+                            <label htmlFor="password">Password</label>
+                            <div className="two">
+                                <input
+                                    type={!passShow ? "password" : "text"}
+                                    value={inpval.password}
+                                    onChange={setVal}
+                                    name="password"
+                                    id="password"
+                                    placeholder='Enter Your Password'
+                                />
+                                <div className="showpass" onClick={() => setPassShow(!passShow)}>
+                                    {!passShow ? "Show" : "Hide"}
+                                </div>
+                            </div>
+                        </div>
+
+                        <button className='btn' onClick={loginuser}>Login</button>
+                        <p>Don't have an Account? <NavLink to="/register">Sign Up</NavLink></p>
+                    </form>
+                    <ToastContainer />
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Login;
